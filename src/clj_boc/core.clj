ea818b115cac2bd803068efee6104d1a56204e41
Salvatore Tosti <saltosti@gmail.com>
2019-05-11 16:23:15 -0700

    Moved UI drawing code into separate file

--- src/clj_boc/core.clj
+++ src/clj_boc/core.clj
@@ -1,81 +1,10 @@
 (ns clj-boc.core
-  (:use [clj-boc.world :only [random-world smooth-world]])
+  (:use [clj-boc.world :only [random-world smooth-world]]
+        [clj-boc.drawing :only [draw-game]])
   (:require [lanterna.screen :as s]))
 
 (defrecord UI [kind])
 (defrecord Game [world uis input])
-
-(def screen-size [80 24])
-
-(defn clear-screen [screen]
-  (let [[cols, rows] screen-size
-         blank (apply str (repeat cols \space))]
-    (doseq [row (range rows)]
-      (s/put-string screen 0 row blank))))
-
-(defn draw-world [screen vrows vcols start-x start-y end-x end-y tiles]
-  (doseq [[vrow-idx mrow-idx] (map vector
-                                   (range 0 vrows)
-                                   (range start-y end-y))
-          :let [row-tiles (subvec (tiles mrow-idx) start-x end-x)]]
-    (doseq [vcol-idx (range vcols)
-            :let [{:keys [glyph color]} (row-tiles vcol-idx)]]
-      (s/put-string screen vcol-idx vrow-idx glyph {:fg color}))))
-
-(defn draw-crosshairs [screen vcols vrows]
-  (let [crosshair-x (int (/ vcols 2))
-        crosshair-y (int (/ vrows 2))]
-    (s/put-string screen crosshair-x crosshair-y "X" {:fg :red})
-    (s/move-cursor screen crosshair-x crosshair-y)))
-
-(defn get-viewport-coords [game vcols vrows]
-  (let [location (:location game)
-        [center-x center-y] location
-        tiles (:tiles (:world game))
-        map-rows (count tiles)
-        map-cols (count (first tiles))
-        start-x (max 0 (- center-x (int (/ vcols 2))))
-        start-y (max 0 (- center-y (int (/ vrows 2))))
-        end-x (+ start-x vcols)
-        end-x (min end-x map-cols)
-        end-y (+ start-y vrows)
-        end-y (min end-y map-rows)
-        start-x (- end-x vcols)
-        start-y (- end-y vrows)]
-        [start-x start-y end-x end-y]))
-
-(defmulti draw-ui
-  (fn [ui game screen]
-    (:kind ui)))
-
-(defmethod draw-ui :start [ui game screen]
-   (s/put-string screen 0 0 "Welcome!")
-   (s/put-string screen 0 1 "Press enter to win or any key to exit..."))
-
-(defmethod draw-ui :play [ui game screen]
-  (let [world (:world game)
-        tiles (:tiles world)
-        [cols rows] screen-size
-        vcols cols
-        vrows (dec rows)
-        [start-x start-y end-x end-y] (get-viewport-coords game vcols vrows)]
-    (draw-world screen vrows vcols start-x start-y end-x end-y tiles)
-    (draw-crosshairs screen vcols vrows)))
-
-(defmethod draw-ui :win [ui game screen]
-  (s/put-string screen 0 0 "Congrats you win!")
-  (s/put-string screen 0 1 "Press any escape to exit, anything else to restart..."))
-
-(defmethod draw-ui :lose [ui game screen]
-  (s/put-string screen 0 0 "Better luck next time")
-  (s/put-string screen 0 1 "Press any escape to exit, anything else to restart..."))
-
-(defn draw-game [game screen]
-  (clear-screen screen)
-  (doseq [ui (:uis game)]
-    (draw-ui ui game screen))
-  (s/redraw screen))
-
 
 (defn move [[x y] [dx dy]]
   [(+ x dx) (+ y dy)])
@@ -87,12 +16,12 @@
 (defmethod process-input :start [game input]
   (-> game
       (assoc :world (random-world))
-      (assoc :uis [(new UI :play)])))
+      (assoc :uis [(->UI :play)])))
 
 (defmethod process-input :play [game input]
   (case input
-    :enter (assoc game :uis [(new UI :win)])
-    :backspace (assoc game :uis [(new UI :lose)])
+    :enter (assoc game :uis [(->UI :win)])
+    :backspace (assoc game :uis [(->UI :lose)])
     \q (assoc game :uis [])
     \m (assoc game :world (smooth-world (:world game)))
 
@@ -106,12 +35,12 @@
 (defmethod process-input :win [game input]
   (if (= input :escape)
     (assoc game :uis [])
-    (assoc game :uis [(new UI :start)])))
+    (assoc game :uis [(->UI :start)])))
 
 (defmethod process-input :lose [game input]
   (if (= input :escape)
     (assoc game :uis [])
-    (assoc game :uis [(new UI :start)])))
+    (assoc game :uis [(->UI :start)])))
 
 (defn get-input [game screen]
   (assoc game :input (s/get-key-blocking screen)))
@@ -125,9 +54,9 @@
         (recur (process-input (dissoc game :input) input))))))
 
 (defn new-game []
-  (assoc (new Game
+  (assoc (->Game
               nil
-              [(new UI :start)]
+              [(->UI :start)]
               nil)
           :location [40 20]))
 
