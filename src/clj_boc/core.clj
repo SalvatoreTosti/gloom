1074260e899dad14457ed984290e0742672add65
Salvatore Tosti <saltosti@gmail.com>
2019-05-11 12:12:18 -0700

    Adds code from caves of clojure post 3.3

--- src/clj_boc/core.clj
+++ src/clj_boc/core.clj
@@ -13,6 +13,37 @@
     (doseq [row (range rows)]
       (s/put-string screen 0 row blank))))
 
+(defn draw-world [screen vrows vcols start-x start-y end-x end-y tiles]
+  (doseq [[vrow-idx mrow-idx] (map vector
+                                   (range 0 vrows)
+                                   (range start-y end-y))
+          :let [row-tiles (subvec (tiles mrow-idx) start-x end-x)]]
+    (doseq [vcol-idx (range vcols)
+            :let [{:keys [glyph color]} (row-tiles vcol-idx)]]
+      (s/put-string screen vcol-idx vrow-idx glyph {:fg color}))))
+
+(defn draw-crosshairs [screen vcols vrows]
+  (let [crosshair-x (int (/ vcols 2))
+        crosshair-y (int (/ vrows 2))]
+    (s/put-string screen crosshair-x crosshair-y "X" {:fg :red})
+    (s/move-cursor screen crosshair-x crosshair-y)))
+
+(defn get-viewport-coords [game vcols vrows]
+  (let [location (:location game)
+        [center-x center-y] location
+        tiles (:tiles (:world game))
+        map-rows (count tiles)
+        map-cols (count (first tiles))
+        start-x (max 0 (- center-x (int (/ vcols 2))))
+        start-y (max 0 (- center-y (int (/ vrows 2))))
+        end-x (+ start-x vcols)
+        end-x (min end-x map-cols)
+        end-y (+ start-y vrows)
+        end-y (min end-y map-rows)
+        start-x (- end-x vcols)
+        start-y (- end-y vrows)]
+        [start-x start-y end-x end-y]))
+
 (defmulti draw-ui
   (fn [ui game screen]
     (:kind ui)))
@@ -21,21 +52,15 @@
    (s/put-string screen 0 0 "Welcome!")
    (s/put-string screen 0 1 "Press enter to win or any key to exit..."))
 
-(defmethod draw-ui :play [ui {{:keys [tiles]} :world :as game} screen]
-  (let [[cols rows] screen-size
+(defmethod draw-ui :play [ui game screen]
+  (let [world (:world game)
+        tiles (:tiles world)
+        [cols rows] screen-size
         vcols cols
         vrows (dec rows)
-        start-x 0
-        start-y 0
-        end-x (+ start-x vcols)
-        end-y (+ start-y vrows)]
-    (doseq [[vrow-idx mrow-idx] (map vector
-                                     (range 0 vrows)
-                                     (range start-y end-y))
-            :let [row-tiles (subvec (tiles mrow-idx) start-x end-x)]]
-      (doseq [vcol-idx (range vcols)
-              :let [{:keys [glyph color]} (row-tiles vcol-idx)]]
-        (s/put-string screen vcol-idx vrow-idx glyph {:fg color})))))
+        [start-x start-y end-x end-y] (get-viewport-coords game vcols vrows)]
+    (draw-world screen vrows vcols start-x start-y end-x end-y tiles)
+    (draw-crosshairs screen vcols vrows)))
 
 (defmethod draw-ui :win [ui game screen]
   (s/put-string screen 0 0 "Congrats you win!")
@@ -51,6 +76,10 @@
     (draw-ui ui game screen))
   (s/redraw screen))
 
+
+(defn move [[x y] [dx dy]]
+  [(+ x dx) (+ y dy)])
+
 (defmulti process-input
   (fn [game input]
     (:kind (last (:uis game)))))
@@ -64,7 +93,14 @@
   (case input
     :enter (assoc game :uis [(new UI :win)])
     :backspace (assoc game :uis [(new UI :lose)])
-    \s (assoc game :world (smooth-world (:world game)))
+    \q (assoc game :uis [])
+    \m (assoc game :world (smooth-world (:world game)))
+
+    \w (update-in game [:location] move [0 -1])
+    \a (update-in game [:location] move [-1 0])
+    \s (update-in game [:location] move [0 1])
+    \d (update-in game [:location] move [1 0])
+
     game))
 
 (defmethod process-input :win [game input]
@@ -89,11 +125,11 @@
         (recur (process-input (dissoc game :input) input))))))
 
 (defn new-game []
-  (new Game
-       nil
-       [(new UI :start)]
-       nil))
-
+  (assoc (new Game
+              nil
+              [(new UI :start)]
+              nil)
+          :location [40 20]))
 
 (defn main
   ([screen-type] (main screen-type false))
