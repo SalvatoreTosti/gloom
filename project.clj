5e4aa81e6df75f480630177fedcfdb14ee87604c
Salvatore Tosti <saltosti@gmail.com>
2019-05-12 11:31:04 -0700

    Updates clojure lanterna version

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clojure-lanterna "0.9.0"]]
+                 [clojure-lanterna "0.9.7"]]
   :main clj-boc.core)
