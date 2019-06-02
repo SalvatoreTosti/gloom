a587cce1ff2526845797c58f96dc7dd0ae1fcfd9
Salvatore Tosti <saltosti@gmail.com>
2019-06-02 11:55:25 -0700

    Adds proof of concept for screen-clear functionality

--- src/gloom/ui/quil_drawing.clj
+++ src/gloom/ui/quil_drawing.clj
@@ -42,10 +42,36 @@
      (when (q/loaded? image)
         (q/image image (* x 16) (* 16 y)))))
 
+(def screen-size [80 24])
+
+(defn clear-screen [tile-map]
+  (let [[cols, rows] screen-size
+        blank (:0 tile-map)]
+    (when (q/loaded? blank)
+      (doall (for [x (range cols)
+                   y (range rows)]
+               (q/image blank (* x 16) (* y 16)))))))
+
+(defn get-viewport-coords [game player-location vcols vrows]
+  (let [location (:location game)
+        [center-x center-y] player-location
+        tiles (:tiles (:world game))
+        map-rows (count tiles)
+        map-cols (count (first tiles))
+        start-x (max 0 (- center-x (int (/ vcols 2))))
+        start-y (max 0 (- center-y (int (/ vrows 2))))
+        end-x (+ start-x vcols)
+        end-x (min end-x map-cols)
+        end-y (+ start-y vrows)
+        end-y (min end-y map-rows)
+        start-x (- end-x vcols)
+        start-y (- end-y vrows)]
+        [start-x start-y end-x end-y]))
+
 (defn setup []
   {:img (q/load-image "resources/monochrome.png") :counter 0})
 
-(defn update [state]
+(defn update-quil [state]
   (update-in state [:counter] inc))
 
 (defn key-pressed [state key-information]
@@ -57,13 +83,14 @@
   (when (q/loaded? (:img state))
     (let [tiles (get-tiles (:img state) 32 32)
           items [{:location [4 6] :tile :10} {:location [6 3] :tile :1001}]]
+      (clear-screen tiles)
       (doall (map #(draw-single-tile tiles %) items)))))
 
 (q/defsketch example
   :title "image demo"
   :size [256 256]
   :setup setup
-  :update update
+  :update update-quil
   :draw draw
   :key-pressed key-pressed
   :middleware [m/fun-mode])
