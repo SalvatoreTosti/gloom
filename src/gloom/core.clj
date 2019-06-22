f6948d10342ec3bfd6f3430e8e99d9926bca4d1f
Salvatore Tosti <saltosti@gmail.com>
2019-06-22 11:43:15 -0700

    Moves old input and drawing files into deprecated directory

--- src/gloom/core.clj
+++ src/gloom/core.clj
@@ -1,7 +1,5 @@
 (ns gloom.core
   (:use [gloom.world :only [random-world smooth-world]]
-        [gloom.ui.drawing :only [draw-game]]
-        [gloom.ui.input :only [get-input process-input]]
         [gloom.ui.core :only [->UI]]
         [gloom.entities.core :only [tick]])
   (:require [lanterna.screen :as s]))
@@ -17,24 +15,24 @@
 (defn clear-messages [game]
   (assoc-in game [:world :entities :player :messages] nil))
 
-(defn run-game [game screen]
-  (loop [{:keys [input uis skip-tick] :as game} game]
-    (when (seq uis)
-      (cond
-        (not (nil? skip-tick))
-        (let [game (assoc-in game [:skip-tick] nil)
-              _ (draw-game game screen)
-              game (clear-messages game)]
-              (recur (get-input game screen)))
+;; (defn run-game [game screen]
+;;   (loop [{:keys [input uis skip-tick] :as game} game]
+;;     (when (seq uis)
+;;       (cond
+;;         (not (nil? skip-tick))
+;;         (let [game (assoc-in game [:skip-tick] nil)
+;;               _ (draw-game game screen)
+;;               game (clear-messages game)]
+;;               (recur (get-input game screen)))
 
-        (nil? input)
-        (let [game (update-in game [:world] tick-all)
-              _ (draw-game game screen)
-              game (clear-messages game)]
-          (recur (get-input game screen)))
+;;         (nil? input)
+;;         (let [game (update-in game [:world] tick-all)
+;;               _ (draw-game game screen)
+;;               game (clear-messages game)]
+;;           (recur (get-input game screen)))
 
-        :else
-        (recur (process-input (dissoc game :input) input))))))
+;;         :else
+;;         (recur (process-input (dissoc game :input) input))))))
 
 (defn new-game [options]
   (->Game
@@ -44,23 +42,23 @@
     nil
     options))
 
-(defn main
-  ([screen-type] (main screen-type false))
-  ([screen-type block?]
-   (letfn [(go []
-               (let [screen (s/get-screen screen-type)]
-                 (s/in-screen screen
-                              (run-game (new-game) screen))))]
-     (if block?
-       (go)
-       (future (go))))))
+;; (defn main
+;;   ([screen-type] (main screen-type false))
+;;   ([screen-type block?]
+;;    (letfn [(go []
+;;                (let [screen (s/get-screen screen-type)]
+;;                  (s/in-screen screen
+;;                               (run-game (new-game) screen))))]
+;;      (if block?
+;;        (go)
+;;        (future (go))))))
 
-(defn -main [& args]
-  (let [args (set args)
-        screen-type (cond
-                      (args ":swing") :swing
-                      (args ":text") :text
-                      :else :auto)]
-    (main screen-type true)))
+;; (defn -main [& args]
+;;   (let [args (set args)
+;;         screen-type (cond
+;;                       (args ":swing") :swing
+;;                       (args ":text") :text
+;;                       :else :auto)]
+;;     (main screen-type true)))
 
 ;; (-main ":swing")
