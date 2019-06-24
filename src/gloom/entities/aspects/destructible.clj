67d0322b4424cf8e863edc408c799027d983d6f3
Salvatore Tosti <saltosti@gmail.com>
2019-06-24 13:45:20 -0700

    Added destroy code hook behavior

--- src/gloom/entities/aspects/destructible.clj
+++ src/gloom/entities/aspects/destructible.clj
@@ -2,10 +2,12 @@
   (:use [gloom.entities.core :only [defaspect]]))
 
 (defaspect Destructible
+  (destroy [{:keys [id] :as this} world]
+           (update-in world [:entities] dissoc id))
   (take-damage [{:keys [id] :as this} damage world]
                (let [damaged-this (update-in this [:hp] - damage)]
                  (if-not (pos? (:hp damaged-this))
-                   (update-in world [:entities] dissoc id)
+                   (destroy this world)
                    (assoc-in world [:entities id] damaged-this))))
   (defense-value [this world]
     (get this :defense 0)))
