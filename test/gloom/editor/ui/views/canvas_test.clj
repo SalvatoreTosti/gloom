640d5e476b983d5538a9b3f3472ce52060a947cf
Salvatore Tosti <saltosti@gmail.com>
2020-01-01 10:38:04 -0500

    Adds basic tests for canvas pickling / unpickling

---
+++ test/gloom/editor/ui/views/canvas_test.clj
@@ -0,0 +1,58 @@
+(ns gloom.editor.ui.views.canvas-test
+  (:require [clojure.test :refer :all]
+            [editor.ui.views.canvas :refer :all]))
+
+
+(defn default-view []
+  {:position [0 0]
+   :width 100
+   :height 100
+   :outline-id :119
+   :cursor-id :787})
+
+(deftest make-canvas-view-base-test
+  (testing "make canvas view base"
+    (are [x y z] (= z (y (make-canvas-view (default-view) x)))
+         {} :kind :canvas
+         {} :position [0 0]
+         {} :width 100
+         {} :height 100
+         {} :outline-id :119
+         {} :cursor-id :787
+         {} :start [1 1]
+         {} :end [99 99])))
+
+(deftest make-canvas-view-canvas-test
+  (testing "make canvas view canvas"
+    (-> (default-view)
+        (make-canvas-view  {})
+        (:canvas)
+        (count)
+        (= (* 98 98))
+        is)))
+
+(deftest pickle-canvas-view-test
+  (testing "pickle canvas view"
+    (are [x y]
+         (= y (x (pickle-canvas-view (make-canvas-view (default-view) {}))))
+         :kind :canvas
+         :position [0 0]
+         :width 100
+         :height 100
+         :outline-id :119
+         :cursor-id :787
+         :pixel-coordinates {:start [0 0], :end [1600 1600]}
+         :palette-view-id nil)))
+
+(deftest unpickle-canvas-view-test
+  (testing "unpickle canvas view"
+    (are [x y]
+         (= y (x (unpickle-canvas-view (pickle-canvas-view (make-canvas-view (default-view) {})))))
+         :kind :canvas
+         :position [0 0]
+         :width 100
+         :height 100
+         :outline-id :119
+         :cursor-id :787
+         :pixel-coordinates {:start [0 0], :end [1600 1600]}
+         :palette-view-id nil)))
