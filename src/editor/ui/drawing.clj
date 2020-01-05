f036f62e298babe39a697edfbd9834dc4bdaf1c1
Salvatore Tosti <saltosti@gmail.com>
2020-01-05 12:46:32 -0500

    Adds consolidated add-dialog function

--- src/editor/ui/drawing.clj
+++ src/editor/ui/drawing.clj
@@ -1,13 +1,13 @@
 (ns editor.ui.drawing
-   (:use
-     [editor.ui.core :only [get-id]]
-     [editor.ui.views.grid :only [make-grid-view]]
-     [editor.ui.views.canvas :only [make-canvas-view]]
-     [editor.ui.views.entity-builder :only [make-entity-builder-view]]
-     [editor.ui.views.dialogs.text-dialog :only [make-text-dialog]]
-     [gloom.ui.core :only [clear-screen draw-tile tile-size]]
-     [gloom.ui.quil-text :only [draw-text draw-text-centered]]
-     )
+  (:use
+    [editor.ui.core :only [get-id]]
+    [editor.ui.views.grid :only [make-grid-view]]
+    [editor.ui.views.canvas :only [make-canvas-view]]
+    [editor.ui.views.entity-builder :only [make-entity-builder-view]]
+    [editor.ui.views.dialogs.text-dialog :only [make-text-dialog]]
+    [gloom.ui.core :only [clear-screen draw-tile tile-size]]
+    [gloom.ui.quil-text :only [draw-text draw-text-centered]]
+    )
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
@@ -19,13 +19,17 @@
         current-x (inc current-x)
         current-y (/ (round-down (q/mouse-y) tile-size) tile-size)
         current-y (inc current-y)]
-  (draw-tile current-x current-y (:tile-map state) :723)))
+    (draw-tile current-x current-y (:tile-map state) :723)))
 
 (defn- add-view [state view]
   (assoc-in state [:editor :views (:id view)] view))
 
 (defn- add-dialog [state view]
-  (assoc-in state [:editor :dialogs (:id view)] view))
+  (let [dialogs (get-in state [:editor :dialogs])
+        dialogs (or dialogs '())]
+    (->> view
+         (conj dialogs)
+         (assoc-in state [:editor :dialogs]))))
 
 (defn make-editor [state]
   (q/frame-rate 30)
@@ -35,7 +39,7 @@
                         :height 24
                         :outline-id :119
                         :cursor-id :787}
-                        state)
+                       state)
         canvas-view (make-canvas-view
                       {:position [9 0]
                        :width 36
@@ -52,25 +56,25 @@
                           :cursor-id :787}
                          state)
         text-dialog (make-text-dialog
-                     {:position [9 12]
-                      :width 20
-                      :height 6
-                      :outline-id :119
-                      :cursor-id :787
-                      :callback-path [:editor :a-test]}
-                     state)]
+                      {:position [9 12]
+                       :width 20
+                       :height 6
+                       :outline-id :119
+                       :cursor-id :787
+                       :callback-path [:editor :a-test]}
+                      state)]
     (-> state
         (add-view palette-view)
         (add-view canvas-view)
         (add-view entity-builder)
-        (assoc-in [:editor :dialogs]
-                  [text-dialog]))))
+        (add-dialog text-dialog) 
+        )))
 
 (defn draw-editor [state]
   (q/no-cursor)
   (clear-screen  (:screen-size state)  (:tile-map state))
   (doseq [view (vals (get-in state [:editor :views]))]
-      ((:draw-fn view) view state))
+    ((:draw-fn view) view state))
   (doseq [view (get-in state [:editor :dialogs])]
-      ((:draw-fn view) view state))
+    ((:draw-fn view) view state))
   (draw-cursor state))
